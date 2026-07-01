fec51cde511092b603541413ff19391778d56a00
AshfakAhmath <ashfakahmath57@gmail.com>
2026-07-01 22:49:21 +0530

    Comment out setTimeout and setInterval functions in hello.js

--- hello.js
+++ hello.js
@@ -4,12 +4,14 @@
 // console.log(window);
 //  console.log(global)
 
-setTimeout(() => {
-  console.log(`Hi! I'm Ashfak Ahmath!`);
-  clearInterval(setIn);
-}, 5000);
+// setTimeout(() => {
+//   console.log(`Hi! I'm Ashfak Ahmath!`);
+//   clearInterval(setIn);
+// }, 5000);
 
-const setIn = setInterval(() => {
-  console.log(`Who are you?`);
-}, 1000);
+// const setIn = setInterval(() => {
+//   console.log(`Who are you?`);
+// }, 1000);
 
+console.log(__dirname);
+console.log(__filename);
